39815205aeb304a358819967c065662f15caab84
Sergio Pomárico <sergiodavid21@gmail.com>
2023-11-30 20:15:19 -0500

    feat: :sparkles: add support to markdown pages

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    published: z.boolean(),
+    author: z.string(),
+    url: z.string().optional(),
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val: string | Date) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog };
