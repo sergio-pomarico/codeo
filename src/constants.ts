39e4a9d0670fd7c739588bbb8b7fde9104944a97
Sergio Pomárico <sergiodavid21@gmail.com>
2023-12-03 08:46:57 -0500

    feat: :mag: improve open graph tags

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
-export const SITE_TITLE = 'codeo';
-export const SITE_DESCRIPTION = '';
+export const SITE_TITLE = "codeo";
+export const SITE_DESCRIPTION = "";
+export const SITE_URL = "https://www.codeo.co";
