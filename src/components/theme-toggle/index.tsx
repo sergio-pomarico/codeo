f6f11e8b3090bec357c6ae0ace7493fc002008fc
Sergio Pomárico <sergiodavid21@gmail.com>
2025-04-17 20:44:34 -0500

    feat: :sparkles: refactor theme toggle component and add styles for improved functionality

---
+++ src/components/theme-toggle/index.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+
+const ThemeToggleButton = () => {
+  const [isMounted, setIsMounted] = useState(false);
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined;
+    }
+    if (typeof localStorage !== undefined && localStorage.getItem("theme")) {
+      return localStorage.getItem("theme");
+    }
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+    return "light";
+  });
+  const toggleTheme = () => {
+    const t = theme === "light" ? "dark" : "light";
+    (
+      document.querySelector("meta[name='theme-color']") as HTMLMetaElement
+    ).content = t === "dark" ? "#18181b" : "#FFF";
+    localStorage.setItem("theme", t);
+    setTheme(t);
+  };
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (theme === "light") {
+      root.classList.remove("dark");
+    } else {
+      root.classList.add("dark");
+    }
+  }, [theme]);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return isMounted ? (
+    <label
+      className="theme-toggle flex items-center justify-center mr-2 text-gray-700 dark:text-gray-100 cursor-pointer"
+      title="Toggle theme"
+    >
+      <input type="checkbox" onChange={toggleTheme} />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        aria-hidden="true"
+        width="2rem"
+        height="2rem"
+        fill="currentColor"
+        stroke-linecap="round"
+        className="theme-toggle__classic"
+        viewBox="0 0 32 32"
+      >
+        <clipPath id="theme-toggle__classic__cutout">
+          <path d="M0-5h30a1 1 0 0 0 9 13v24H0Z" />
+        </clipPath>
+        <g clip-path="url(#theme-toggle__classic__cutout)">
+          <circle cx="16" cy="16" r="9.34" />
+          <g stroke="currentColor" strokeWidth="1.5">
+            <path d="M16 5.5v-4" />
+            <path d="M16 30.5v-4" />
+            <path d="M1.5 16h4" />
+            <path d="M26.5 16h4" />
+            <path d="m23.4 8.6 2.8-2.8" />
+            <path d="m5.7 26.3 2.9-2.9" />
+            <path d="m5.8 5.8 2.8 2.8" />
+            <path d="m23.4 23.4 2.9 2.9" />
+          </g>
+        </g>
+      </svg>
+    </label>
+  ) : (
+    <div />
+  );
+};
+
+export default ThemeToggleButton;
